9abea58204a5693489f11a9ac9d28246422096d9
Barthol1 <barthol2003@gmail.com>
2025-12-07 15:50:32 +0100

    Solution for day05-2 and day06

---
+++ Day05-2/Program.cs
@@ -0,0 +1,48 @@
+﻿// We want to know how many fresh ID's are available
+// An ID is fresh if it is contained in any range
+// The ranges are inclusive
+
+var input = File.ReadAllLines("input.txt").ToArray();
+int seperatorIndex = Array.IndexOf(input, "");
+
+var ranges = input.Take(seperatorIndex).ToArray();
+
+// Convert the ranges to start/end objects
+List<(long start, long end)> rangeList = [];
+foreach (var range in ranges)
+{
+    rangeList.Add((long.Parse(range.Split('-')[0]), long.Parse(range.Split('-')[1])));
+}
+
+// Sort them by ascending start value
+rangeList = rangeList.OrderBy(s => s.start).ToList();
+
+// Take the first interval as current
+// Loop through each interval, if the current end is bigger than the next start, than the next end = current end
+// The base range at the end of this loop should be all fresh ID's
+
+long totalCount = 0;
+var currentInterval = rangeList[0];
+
+for (int i = 1; i < rangeList.Count; i++)
+{
+    var next = rangeList[i];
+
+    // If the current current end is bigger or equal to next start, merge the next end into the current
+    if(currentInterval.end >= next.start)
+    {
+        currentInterval.end = Math.Max(currentInterval.end, next.end);
+    } 
+
+    // Else move on to the next range and append the amount of possible id's to the result
+    else 
+    {
+        // add 1 to the end since the ranges are inclusive
+        totalCount += currentInterval.end + 1 - currentInterval.start;
+        currentInterval = next;
+    }
+}
+
+//Add last range
+totalCount += currentInterval.end + 1 - currentInterval.start;
+Console.WriteLine(totalCount);
---
+++ Day06/Program.cs
@@ -0,0 +1,62 @@
+﻿// Group of numbers either added or multiplied
+// Problems are seperated by spaces
+// At the bottom of the problem the symbol for the operation is located
+// We need to return the total of the results to the problems
+
+// quick reminders
+// A number doesn't have to begin on the first index
+// Operation symbol is always located on the first index of row 5 for each column
+
+var lines = File.ReadLines("input.txt").ToArray();
+
+string[][] parseProblems(string[] lines)
+{
+    int columnCount = lines.First().Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Length;
+    string[][] problems = new string[columnCount][];
+
+    // Initialize all column arrays
+    for (int i = 0; i < columnCount; i++)
+    {
+        problems[i] = Array.Empty<string>();
+    }
+
+    foreach(var line in lines)
+    {
+        // get each value for a row
+        var row = line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        for (int i = 0; i < row.Length; i++)
+        {
+            problems[i] = problems[i].Append(row[i]).ToArray();
+        }
+    }
+
+    return problems;
+}
+
+var problems = parseProblems(lines);
+long totalResult = 0;
+foreach(var problem in problems)
+{
+    // Last value is always the symbol
+    var symbol = problem.Last();
+
+    // Determine base result based on symbol
+    long problemResult = (symbol == "*") ? 1 : 0;
+
+    for (int i = 0; i < problem.Length - 1; i++)
+    {
+        // Perform operation
+        switch(symbol)
+        {
+            case "+":
+                problemResult+=int.Parse(problem[i]);
+                break;
+            case "*":
+                problemResult*=int.Parse(problem[i]);
+                break;
+        }
+    }
+    totalResult+=problemResult;
+}
+
+Console.WriteLine(totalResult);
